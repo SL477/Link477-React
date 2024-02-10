b9def1bd8f6353569db1a6d10e2efc151fe89c9b
Tom Fishwick <fishwickt@gmail.com>
2024-02-10 16:09:34 +0000

    Arrays file

---
+++ src/Arrays.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+export default function StudentGrades() {
+    const [currentGrade, setCurrentGrade] = useState('');
+    const [grades, setGrades] = useState<number[]>([]);
+    const words = ['The','quick','brown','fox','jumps','over','the','lazy','dog'];
+
+    const currentGradeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentGrade(e.target.value);
+    };
+
+    const gradeAdder = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        const arr = [...grades];
+        if (currentGrade) {
+            arr.push(parseFloat(currentGrade));
+            setGrades(arr);
+        }
+    };
+
+    return (
+        <div>
+            <p>My answers to the exercises in <a href="https://github.com/GauravWalia19/Free-Algorithms-Books/blob/master/Library/src/JAVASCRIPT/Data-Structures-%26-Algorithms-with-JavaScript.pdf">Algorithms &amp; Data Structures with JS</a></p>
+            <fieldset>
+                <label>Add Grade:
+                    <input name='addGrade' type='number' className='form-control' value={currentGrade} onChange={currentGradeChange}/>
+                </label>
+                <button type='button' className='btn btn-primary' onClick={gradeAdder}>Add Grade</button>
+            </fieldset>
+            <p>Grades:</p>
+            <ul>
+                {grades.map((grade, idx) => <li key={idx}>{grade}</li>)}
+            </ul>
+            <p>Average Grade: {grades.length !== 0? grades.reduce((a, b) => a + b, 0) / grades.length : 0}</p>
+            <p><b>2. Words</b></p>
+            <p>{words.toString()}</p>
+            <p>{words.reverse().toString()}</p>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import StudentGrades from './Arrays';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './main.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Test</div>
+  },
+  {
+    path: '/arrays',
+    element: <StudentGrades/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
