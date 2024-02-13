714482705d940234a4282fce212645293d287876
Tom Fishwick <fishwickt@gmail.com>
2024-02-13 21:12:52 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {modules: {localsConvention: 'camelCase'}}
+  css: {modules: {localsConvention: 'camelCase'}},
+  base: "/Link477-React/"
 })
