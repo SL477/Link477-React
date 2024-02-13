b447ed00e435aeab3bad889e76d825190858c996
Tom Fishwick <fishwickt@gmail.com>
2024-02-13 20:59:18 +0000

    Update tictactoe.tsx

--- src/ticTacToe/tictactoe.tsx
+++ src/ticTacToe/tictactoe.tsx
@@ -1,105 +1,105 @@
-import React from "react";
+import { useState } from "react";
+import classes from './tictactoe.module.css';
 
-export default class TicTacToe extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentGrid: [0,1,2,3,4,5,6,7,8],//["X", 1, "O", "X", 4, "X", "O", "O", 8],
-            aiMark: '',
-            humanMark: '',
-            block: false,
-            message: '',
-            humanScore: 0,
-            aiScore: 0,
-        };
+export default function TicTacToe() {
+    const [currentGrid, setCurrentGrid] = useState<(number|string)[]>([0,1,2,3,4,5,6,7,8]);
+    const [AIMark, setAIMark] = useState('');
+    const [humanMark, setHumanMark] = useState('');
+    const [block, setBlock] = useState(false);
+    const [message, setMessage] = useState('');
+    const [humanScore, setHumanScore] = useState(0);
+    const [AIScore, setAIScore] = useState(0);
 
-        this.displayValue = this.displayValue.bind(this);
-        this.getAllEmptyCellsIndexes = this.getAllEmptyCellsIndexes.bind(this);
-        this.checkIfWinnerFound = this.checkIfWinnerFound.bind(this);
-        this.minimax = this.minimax.bind(this);
-        this.humanMove = this.humanMove.bind(this);
-        this.resetGame = this.resetGame.bind(this);
+    const marks = {
+        x: 'X',
+        o: 'O'
+    };
+
+    const displayValue = (index: number) => {
+        if (currentGrid[index] === marks.x || currentGrid[index] === marks.o) {
+            return currentGrid[index];
+        }
+        return ' ';
+    };
+
+    /**
+     * @returns {number[]}
+     */
+    const getAllEmptyCellsIndexes = (currentBoardState: (number|string)[]): number[] => {
+        // step 4
+        return currentBoardState.filter(i => i !== marks.x && i !== marks.o).map(i => Number(i));
+    };
+
+    const checkIfWinnerFound = (currentBoardState: (number|string)[], currentMark: string) => {
+        // step 5
+        return (currentBoardState[0] === currentMark && currentBoardState[1] === currentMark && currentBoardState[2] === currentMark) ||
+        (currentBoardState[3] === currentMark && currentBoardState[4] === currentMark && currentBoardState[5] === currentMark) ||
+        (currentBoardState[6] === currentMark && currentBoardState[7] === currentMark && currentBoardState[8] === currentMark) ||
+        (currentBoardState[0] === currentMark && currentBoardState[3] === currentMark && currentBoardState[6] === currentMark) ||
+        (currentBoardState[1] === currentMark && currentBoardState[4] === currentMark && currentBoardState[7] === currentMark) ||
+        (currentBoardState[2] === currentMark && currentBoardState[5] === currentMark && currentBoardState[8] === currentMark) ||
+        (currentBoardState[0] === currentMark && currentBoardState[4] === currentMark && currentBoardState[8] === currentMark) ||
+        (currentBoardState[2] === currentMark && currentBoardState[4] === currentMark && currentBoardState[6] === currentMark)
+    };
+
+    interface testPlayInfo {
+        score: number;
+        index?: number|string;
     }
 
-    displayValue(index) {
-        if (this.state.currentGrid[index] === 'X' || this.state.currentGrid[index] === 'O') {
-            return this.state.currentGrid[index];
-        }
-        else {
-            return ' ';
-        }
-    }
-
-    getAllEmptyCellsIndexes(currBdSt) {
-        //Step 4
-        return currBdSt.filter(i => i !== 'X' && i !== 'O');
-    }
-
-    checkIfWinnerFound(currBdSt, currMark) {
-        //Step 5
-        if (
-            (currBdSt[0] === currMark && currBdSt[1] === currMark && currBdSt[2] === currMark) ||
-            (currBdSt[3] === currMark && currBdSt[4] === currMark && currBdSt[5] === currMark) ||
-            (currBdSt[6] === currMark && currBdSt[7] === currMark && currBdSt[8] === currMark) ||
-            (currBdSt[0] === currMark && currBdSt[3] === currMark && currBdSt[6] === currMark) ||
-            (currBdSt[1] === currMark && currBdSt[4] === currMark && currBdSt[7] === currMark) ||
-            (currBdSt[2] === currMark && currBdSt[5] === currMark && currBdSt[8] === currMark) ||
-            (currBdSt[0] === currMark && currBdSt[4] === currMark && currBdSt[8] === currMark) ||
-            (currBdSt[2] === currMark && currBdSt[4] === currMark && currBdSt[6] === currMark) 
-        ) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    minimax(currBdSt, currMark) {
-        //Step 8
-        const availCellsIndexes = this.getAllEmptyCellsIndexes(currBdSt);
-
-        //Step 9, check if terminal state
-        if (this.checkIfWinnerFound(currBdSt, this.state.humanMark)) {
+    /**
+     * @returns {testPlayInfo}
+     */
+    const miniMax = (currentBoardStateOld: (number|string)[], currentMark: string): testPlayInfo => {
+        const currentBoardState = [...currentBoardStateOld];
+        // Step 8
+        const availableCellsIndexes = getAllEmptyCellsIndexes(currentBoardState);
+        
+        // step 9, check if terminal state
+        if (checkIfWinnerFound(currentBoardState, humanMark)) {
             return {score: -1};
         }
-        else if (this.checkIfWinnerFound(currBdSt, this.state.aiMark)) {
+        else if (checkIfWinnerFound(currentBoardState, AIMark)) {
             return {score: 1};
         }
-        else if (availCellsIndexes.length === 0) {
+        else if (availableCellsIndexes.length === 0) {
             return {score: 0};
         }
 
-        //Step 10
-        const allTestPlayInfos = [];
+        // step 10
+        const allTestPlayInfos: testPlayInfo[] = [];
 
-        //Step 11
-        for (let i = 0; i < availCellsIndexes.length; i++) {
-            const currentTestPlayInfo = {};
-            currentTestPlayInfo['index'] = currBdSt[availCellsIndexes[i]];
-            currBdSt[availCellsIndexes[i]] = currMark;
+        // step 11
+        for (let i = 0; i < availableCellsIndexes.length; i++) {
+            const currentTestPlayInfo: testPlayInfo = {
+                score: 0,
+            };
+            currentTestPlayInfo.index = currentBoardState[availableCellsIndexes[i]];
+            currentBoardState[availableCellsIndexes[i]] = currentMark;
+            // console.log('currentBoardState', currentBoardState, availableCellsIndexes[i]);
 
-            if (currMark === this.state.aiMark) {
-                const result = this.minimax(currBdSt, this.state.humanMark);
+            if (currentMark === AIMark) {
+                const result = miniMax(currentBoardState, humanMark);
                 currentTestPlayInfo['score'] = result['score'];
             }
             else {
-                const result = this.minimax(currBdSt, this.state.aiMark);
+                const result = miniMax(currentBoardState, AIMark);
                 currentTestPlayInfo['score'] = result['score'];
             }
 
-            //Step 12
-            currBdSt[availCellsIndexes[i]] = currentTestPlayInfo.index;
+            // step 12
+            currentBoardState[availableCellsIndexes[i]] = currentTestPlayInfo.index;
 
             allTestPlayInfos.push(currentTestPlayInfo);
         }
 
-        //Step 14
+        // step 14
 
-        //Step 15
-        let bestTestPlay = null;
+        // step 15
+        let bestTestPlay = 0;
 
-        //Step 16
-        if (currMark === this.state.aiMark) {
+        // step 16
+        if (currentMark === AIMark) {
             let bestScore = -Infinity;
             for (let i = 0; i < allTestPlayInfos.length; i++) {
                 if (allTestPlayInfos[i].score > bestScore) {
@@ -117,55 +117,62 @@ export default class TicTacToe extends React.Component {
                 }
             }
         }
-
-        //Step 17
+        // step 17
         return allTestPlayInfos[bestTestPlay];
-    }
+    };
 
-    resetGame() {
-        //delay 5 seconds and then restart
-        this.setState({block: true});
+    const resetGame = () => {
+        setBlock(true);
+        // delay 3 seconds and then restart
         setTimeout(() => {
-            this.setState({currentGrid: [0,1,2,3,4,5,6,7,8], block: false, message: ''});
+            setCurrentGrid([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+            setBlock(false);
+            setMessage('');
         }, 3000);
-    }
+    };
 
-    humanMove(index) {
-        if (this.state.block) {
-            //waiting for the computer to move
+    const humanMove = (index: number) => {
+        if (block) {
+            // waiting for computer to move
             return;
         }
         console.log('test', index);
-        if (this.displayValue(index) === ' ') {
-            let currBdSt = [...this.state.currentGrid];
-            currBdSt[index] = this.state.humanMark;
-            this.setState({currentGrid: currBdSt, block: true});
-            if (this.checkIfWinnerFound(currBdSt, this.state.humanMark)) {
-                //alert('You won!');
-                this.setState({message: 'You won!', humanScore: (this.state.humanScore + 1)});
-                this.resetGame();
+        if (displayValue(index) === ' ') {
+            const currentBoardState = [...currentGrid];
+            currentBoardState[index] = humanMark;
+            setCurrentGrid(currentBoardState);
+            setBlock(true);
+
+            if (checkIfWinnerFound(currentBoardState, humanMark)) {
+                setMessage('You won!');
+                setHumanScore(humanScore + 1);
+                resetGame();
             }
-            else if (this.getAllEmptyCellsIndexes(currBdSt).length === 0) {
-                //alert('Draw!');
-                this.setState({message: 'Draw!'});
-                this.resetGame();
+            else if (getAllEmptyCellsIndexes(currentBoardState).length === 0) {
+                // draw
+                setMessage('Draw!');
+                resetGame();
             }
             else {
-                //console.log(this.minimax(currBdSt, this.state.aiMark));
                 setTimeout(() => {
-                    let aiMove = this.minimax(currBdSt, this.state.aiMark);
+                    const aiMove = miniMax(currentBoardState, AIMark);
                     console.log(aiMove);
-                    currBdSt[aiMove.index] = this.state.aiMark;
-                    this.setState({currentGrid: currBdSt, block: false});
-                    if (this.checkIfWinnerFound(currBdSt, this.state.aiMark)) {
-                        //alert('You lost!');
-                        this.setState({message: 'You lost!', aiScore: (this.state.aiScore + 1)});
-                        this.resetGame();
+                    if (aiMove.index && typeof aiMove.index === 'number') {
+                        currentBoardState[aiMove.index] = AIMark;
                     }
-                    else if (this.getAllEmptyCellsIndexes(currBdSt).length === 0) {
-                        //alert('Draw!');
-                        this.setState({message: 'Draw!'});
-                        this.resetGame();
+                    else {
+                        currentBoardState[getAllEmptyCellsIndexes(currentBoardState)[0]] = AIMark;
+                    }
+                    setCurrentGrid(currentBoardState);
+                    setBlock(false);
+                    if (checkIfWinnerFound(currentBoardState, AIMark)) {
+                        setMessage('You lost!');
+                        setAIScore(AIScore + 1);
+                        resetGame();
+                    }
+                    else if (getAllEmptyCellsIndexes(currentBoardState).length === 0) {
+                        setMessage('Draw!');
+                        resetGame();
                     }
                 }, 1000);
             }
@@ -173,67 +180,59 @@ export default class TicTacToe extends React.Component {
         else {
             console.log('Position filled');
         }
-    }
+    };
 
-    render() {
-        let disp;
-        if (this.state.humanMark === '') {
-            disp = (
-                <div className="centre">
-                    <p className="choose">Choose your side:</p>
-                    <button className="btn btn-primary" onClick={() =>this.setState({aiMark: 'O', humanMark: 'X'})}>X</button>
-                    <button className="btn btn-primary" onClick={() =>this.setState({aiMark: 'X', humanMark: 'O'})}>O</button>
-                </div>
-            );
-        }
-        else {
-            disp = (
-                <div className="centre">
-                    <div className="tbl">
+    return (
+        <div className={classes.centre}>
+            {humanMark === ''? (
+                <>
+                    <p className={classes.choose}>Choose your side:</p>
+                    <button className="btn btn-primary" onClick={() => {setAIMark(marks.o); setHumanMark(marks.x);}} type="button">X</button>
+                    <button className="btn btn-primary" onClick={() => {setAIMark(marks.x); setHumanMark(marks.o);}} type="button">O</button>
+                </>
+            ) : (
+                <>
+                    <div className={classes.tbl}>
                         <table className="table table-bordered">
                             <tbody>
                                 <tr>
-                                    <td className="td" onClick={() =>{this.humanMove(0);}}>{this.displayValue(0)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(1);}}>{this.displayValue(1)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(2);}}>{this.displayValue(2)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(0)}>{displayValue(0)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(1)}>{displayValue(1)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(2)}>{displayValue(2)}</td>
                                 </tr>
                                 <tr>
-                                    <td className="td" onClick={() =>{this.humanMove(3);}}>{this.displayValue(3)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(4);}}>{this.displayValue(4)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(5);}}>{this.displayValue(5)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(3)}>{displayValue(3)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(4)}>{displayValue(4)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(5)}>{displayValue(5)}</td>
                                 </tr>
                                 <tr>
-                                    <td className="td" onClick={() =>{this.humanMove(6);}}>{this.displayValue(6)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(7);}}>{this.displayValue(7)}</td>
-                                    <td className="td" onClick={() =>{this.humanMove(8);}}>{this.displayValue(8)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(6)}>{displayValue(6)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(7)}>{displayValue(7)}</td>
+                                    <td className={classes.td} onClick={() => humanMove(8)}>{displayValue(8)}</td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
-                    {/*Step 7*/}
-                    {/*<button className="btn btn-primary" onClick={ () => console.log(this.minimax(this.state.currentGrid, this.state.aiMark))}>MiniMax</button>*/}
                     <br/>
-                    <div className="scoresTbl">
+                    <div className={classes.scoresTbl}>
                         <table className="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th className="centre">Your Score</th>
-                                    <th className="centre">Computer Score</th>
+                                    <th className={`${classes.th} ${classes.centre}`}>Your Score</th>
+                                    <th className={`${classes.th} ${classes.centre}`}>Computer Score</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>{this.state.humanScore}</td>
-                                    <td>{this.state.aiScore}</td>
+                                    <td>{humanScore}</td>
+                                    <td>{AIScore}</td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
-                    <p>{this.state.message}</p>
-                </div>
-            );
-        }
-
-        return (<div>{disp}</div>);
-    }
+                    <p>{message}</p>
+                </>
+            )}
+        </div>
+    );
 }
