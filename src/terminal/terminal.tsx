aad53993becab88a80741119f30652e3e5cbdd10
Tom Fishwick <fishwickt@gmail.com>
2024-05-25 18:06:01 +0100

    Added in LS and CD

--- src/terminal/terminal.tsx
+++ src/terminal/terminal.tsx
@@ -14,10 +14,30 @@ export default function Terminal() {
   const [terminalText, setTerminalText] = useState<string | null>('');
   const [terminalHistory, setTerminalHistory] = useState<string[]>([]);
   const [terminalOutput, setTerminalOutput] = useState<string[][]>([]);
+  const [terminalPath, setTerminalPath] = useState('~');
 
   const commands: { [index: string]: string } = {
+    cd: 'Change Directory',
     clear: 'Wipes the input',
     help: 'Shows the commands',
+    ls: 'List the directory structure',
+  };
+
+  const fileDirectory: { [index: string]: string } = {
+    education: `education
+* Oxford Brookes: "Mathematics" 2009-2012
+* City University of London: "Data Science" 2021-2022`,
+    skills: `languages
+* JavaScrip/TypeScript
+* Python
+* SQL
+* C#
+* VB.Net
+* XSLT
+
+tools
+* Docker
+* Git`,
   };
 
   const clearTerminal = () => {
@@ -38,7 +58,102 @@ export default function Terminal() {
         for (const key in commands) {
           helpOutput += `${key}: ${commands[key]}\n`;
         }
-        newTerminalOutput.push([terminalText, helpOutput]);
+        newTerminalOutput.push([terminalText, helpOutput, terminalPath]);
+        setTerminalOutput(newTerminalOutput);
+      } else if (
+        terminalText.toLowerCase().startsWith('ls ') ||
+        terminalText.toLowerCase() === 'ls'
+      ) {
+        const splitTerminalText = terminalText.toLowerCase().split(' ');
+        let requestedDirectory = terminalPath;
+        if (splitTerminalText.length > 1) {
+          requestedDirectory = splitTerminalText[1];
+        }
+
+        let lsOutput = '';
+        let isError = false;
+        if (['~', '..'].includes(requestedDirectory)) {
+          for (const key in fileDirectory) {
+            lsOutput += `${key}\n`;
+          }
+        } else if (
+          requestedDirectory.startsWith('~/') &&
+          requestedDirectory.replace('~/', '') in fileDirectory
+        ) {
+          lsOutput += fileDirectory[requestedDirectory.replace('~/', '')];
+        } else if (
+          terminalPath === '~' &&
+          requestedDirectory in fileDirectory
+        ) {
+          lsOutput += fileDirectory[requestedDirectory];
+        } else {
+          lsOutput += 'Invalid directory';
+          isError = true;
+        }
+
+        const newTerminalOutput = [...terminalOutput];
+        if (isError) {
+          newTerminalOutput.push([
+            terminalText,
+            lsOutput,
+            terminalPath,
+            styles.redText,
+          ]);
+        } else {
+          newTerminalOutput.push([terminalText, lsOutput, terminalPath]);
+        }
+        setTerminalOutput(newTerminalOutput);
+      } else if (
+        terminalText.toLowerCase() === 'cd' ||
+        terminalText.toLowerCase().startsWith('cd ')
+      ) {
+        const splitTerminalText = terminalText.toLowerCase().split(' ');
+        if (splitTerminalText.length > 1) {
+          // Changing
+          let newDirectory = '';
+          const requestedDirectory = splitTerminalText[1];
+          if (requestedDirectory === '..' && terminalPath !== '~') {
+            newDirectory = '~';
+          } else if (
+            terminalPath === '~' &&
+            requestedDirectory in fileDirectory
+          ) {
+            newDirectory = `~/${requestedDirectory}`;
+          } else if (
+            terminalPath.startsWith('~/') &&
+            requestedDirectory.startsWith('~/') &&
+            requestedDirectory.replace('~/', '') in fileDirectory
+          ) {
+            newDirectory = requestedDirectory;
+          }
+
+          const newTerminalOutput = [...terminalOutput];
+          if (newDirectory === '') {
+            newTerminalOutput.push([
+              terminalText,
+              'Wrong directory',
+              terminalPath,
+              styles.redText,
+            ]);
+          } else {
+            newTerminalOutput.push([terminalText, '', terminalPath]);
+            setTerminalPath(newDirectory);
+          }
+          setTerminalOutput(newTerminalOutput);
+        } else {
+          // no change
+          const newTerminalOutput = [...terminalOutput];
+          newTerminalOutput.push([terminalText, '', terminalPath]);
+          setTerminalOutput(newTerminalOutput);
+        }
+      } else {
+        const newTerminalOutput = [...terminalOutput];
+        newTerminalOutput.push([
+          terminalText,
+          `Command '${terminalText}' Not Found!`,
+          terminalPath,
+          styles.redText,
+        ]);
         setTerminalOutput(newTerminalOutput);
       }
 
@@ -46,6 +161,10 @@ export default function Terminal() {
       newHist.push(terminalText);
       setTerminalHistory(newHist);
       clearTerminal();
+    } else {
+      const newTerminalOutput = [...terminalOutput];
+      newTerminalOutput.push(['', '', terminalPath]);
+      setTerminalOutput(newTerminalOutput);
     }
   };
 
@@ -58,17 +177,20 @@ export default function Terminal() {
           <>
             <span className={styles.terminalLabel}>
               guest@link477.com<span className={styles.greyText}>:</span>
-              <span className={styles.blueText}>~</span>
+              <span className={styles.blueText}>{priorCmd[2]}</span>
               <span className={styles.greyText}>$</span>&nbsp;
             </span>
             <span>{priorCmd[0]}</span>
-            <pre>{priorCmd[1]}</pre>
+
+            <pre className={priorCmd.length >= 4 ? priorCmd[3] : ''}>
+              {priorCmd[1]}
+            </pre>
           </>
         );
       })}
       <span className={styles.terminalLabel}>
         guest@link477.com<span className={styles.greyText}>:</span>
-        <span className={styles.blueText}>~</span>
+        <span className={styles.blueText}>{terminalPath}</span>
         <span className={styles.greyText}>$</span>&nbsp;
       </span>
       <span
