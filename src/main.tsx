112ff5f354cf2e9466e51c0026b23d538c31b6fb
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-02-15 17:46:17 +0000

    Import new navbar and start light-dark

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import StudentGrades from './Arrays';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './main.css';
 import Weather from './weather/Weather';
 import Battleship from './battleship/battleship';
@@ -16,46 +16,16 @@ import Terminal from './terminal/terminal';
 import RecipeBox from './recipeBox/RecipeBox';
 
 const router = createHashRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/arrays',
-    element: <StudentGrades />,
-  },
-  {
-    path: '/weather',
-    element: <Weather />,
-  },
-  {
-    path: '/battleship',
-    element: <Battleship />,
-  },
-  {
-    path: '/gameoflife',
-    element: <GameOfLife />,
-  },
-  {
-    path: '/rockPaperScissors',
-    element: <RockPaperScissors />,
-  },
-  {
-    path: '/tictactoe',
-    element: <TicTacToe />,
-  },
-  {
-    path: '/santaTracker',
-    element: <SantaTracker />,
-  },
-  {
-    path: 'terminal',
-    element: <Terminal />,
-  },
-  {
-    path: 'recipeBox',
-    element: <RecipeBox />,
-  },
+  { path: '/', element: <Home /> },
+  { path: '/arrays', element: <StudentGrades /> },
+  { path: '/weather', element: <Weather /> },
+  { path: '/battleship', element: <Battleship /> },
+  { path: '/gameoflife', element: <GameOfLife /> },
+  { path: '/rockPaperScissors', element: <RockPaperScissors /> },
+  { path: '/tictactoe', element: <TicTacToe /> },
+  { path: '/santaTracker', element: <SantaTracker /> },
+  { path: 'terminal', element: <Terminal /> },
+  { path: 'recipeBox', element: <RecipeBox /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
