bd9d155dcf2856c6b5a040ab5e87cb73a4a88bdf
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-10-19 14:18:00 +0100

    Sorted cards

--- src/memoryCard/memoryCard.tsx
+++ src/memoryCard/memoryCard.tsx
@@ -1,49 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classes from './memoryCard.module.css';
+
+class imgCard {
+  id: number;
+  name: string;
+  path: string;
+  sort: number;
+  constructor(id: number, name: string, path: string) {
+    this.id = id;
+    this.name = name;
+    this.path = path;
+    this.sort = Math.random();
+  }
+}
+
 export default function MemoryCard() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0); //TODO: save this to local storage
   const [clickedImgs, setClickedImgs] = useState<number[]>([]);
+  const [newAntiochTeam, setNewAntiochTeam] = useState<imgCard[]>([]);
 
-  const newAntiochTeam = [
-    {
-      name: 'Engineer George',
-      path: 'EngineerGeorge.jpg',
-      id: 1,
-      sort: Math.random(),
-    },
-    {
-      name: 'Brother Julius',
-      path: 'BrotherJulius.jpg',
-      id: 2,
-      sort: Math.random(),
-    },
-    {
-      name: 'Lt Carstairs',
-      path: 'LtCarstairs.jpg',
-      id: 3,
-      sort: Math.random(),
-    },
-    {
-      name: 'Preacher Peter',
-      path: 'PreacherPeter.jpg',
-      id: 4,
-      sort: Math.random(),
-    },
-    {
-      name: 'Private Graham',
-      path: 'PrivateGraham.jpg',
-      id: 5,
-      sort: Math.random(),
-    },
-    {
-      name: 'Brother Mathius',
-      path: 'BrotherMathius.jpg',
-      id: 6,
-      sort: Math.random(),
-    },
-    { name: 'The Reaper', path: 'TheReaper.jpg', id: 7, sort: Math.random() },
-  ];
+  useEffect(
+    () =>
+      setNewAntiochTeam(
+        [
+          new imgCard(1, 'Engineer George', 'EngineerGeorge.jpg'),
+          new imgCard(2, 'Brother Julius', 'BrotherJulius.jpg'),
+          new imgCard(3, 'Lt Carstairs', 'LtCarstairs.jpg'),
+          new imgCard(4, 'Preacher Peter', 'PreacherPeter.jpg'),
+          new imgCard(5, 'Private Graham', 'PrivateGraham.jpg'),
+          new imgCard(6, 'Brother Matheus', 'BrotherMatheus.jpg'),
+          new imgCard(7, 'The Reaper', 'TheReaper.jpg'),
+        ].sort((i, j) => (i.sort > j.sort ? 1 : -1))
+      ),
+    []
+  );
 
   const clickedImg = (imgId: number) => {
     if (clickedImgs.includes(imgId)) {
@@ -56,6 +47,15 @@ export default function MemoryCard() {
       setScore(score + 1);
       setClickedImgs([...clickedImgs, imgId]);
     }
+    // resort
+    setNewAntiochTeam(
+      newAntiochTeam
+        .map((i) => {
+          i.sort = Math.random();
+          return i;
+        })
+        .sort((i, j) => (i.sort > j.sort ? 1 : -1))
+    );
   };
 
   return (
@@ -73,18 +73,16 @@ export default function MemoryCard() {
         </div>
       </div>
       <div className={classes.pictureGrid}>
-        {newAntiochTeam
-          .sort((i) => i.sort)
-          .map((i) => (
-            <div
-              key={i.id}
-              className={classes.pictureHolder}
-              onClick={() => clickedImg(i.id)}
-            >
-              <img src={`/Link477-React/${i.path}`} alt={i.name} />
-              <p>{i.name}</p>
-            </div>
-          ))}
+        {newAntiochTeam.map((i) => (
+          <div
+            key={i.id}
+            className={classes.pictureHolder}
+            onClick={() => clickedImg(i.id)}
+          >
+            <img src={`/Link477-React/${i.path}`} alt={i.name} />
+            <p>{i.name}</p>
+          </div>
+        ))}
       </div>
     </main>
   );
