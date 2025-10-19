59a9244b3f2fbc6cc278293f6c402456dff8b957
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-10-19 11:56:14 +0100

    Started the UI for the picture grid

--- src/memoryCard/memoryCard.tsx
+++ src/memoryCard/memoryCard.tsx
@@ -3,6 +3,47 @@ import classes from './memoryCard.module.css';
 export default function MemoryCard() {
   const [score] = useState(0);
   const [bestScore] = useState(0); //TODO: save this to local storage
+
+  const newAntiochTeam = [
+    {
+      name: 'Engineer George',
+      path: 'EngineerGeorge.jpg',
+      id: 1,
+      sort: Math.random(),
+    },
+    {
+      name: 'Brother Julius',
+      path: 'BrotherJulius.jpg',
+      id: 2,
+      sort: Math.random(),
+    },
+    {
+      name: 'Lt Carstairs',
+      path: 'LtCarstairs.jpg',
+      id: 3,
+      sort: Math.random(),
+    },
+    {
+      name: 'Preacher Peter',
+      path: 'PreacherPeter.jpg',
+      id: 4,
+      sort: Math.random(),
+    },
+    {
+      name: 'Private Graham',
+      path: 'PrivateGraham.jpg',
+      id: 5,
+      sort: Math.random(),
+    },
+    {
+      name: 'Brother Mathius',
+      path: 'BrotherMathius.jpg',
+      id: 6,
+      sort: Math.random(),
+    },
+    { name: 'The Reaper', path: 'TheReaper.jpg', id: 7, sort: Math.random() },
+  ];
+
   return (
     <main>
       <h1 className="centertext">Memory Game</h1>
@@ -16,6 +57,16 @@ export default function MemoryCard() {
           <br />
           <span>Best Score: {bestScore}</span>
         </div>
+      </div>
+      <div className={classes.pictureGrid}>
+        {newAntiochTeam
+          .sort((i) => i.sort)
+          .map((i) => (
+            <div key={i.id} className={classes.pictureHolder}>
+              <img src={`/Link477-React/${i.path}`} alt={i.name} />
+              <p>{i.name}</p>
+            </div>
+          ))}
       </div>
     </main>
   );
