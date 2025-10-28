396ef354692cbfe185bdbf7e51d1292dcebb72d1
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-10-28 11:47:10 +0000

    Setup Navigation

--- src/shop/shop.tsx
+++ src/shop/shop.tsx
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import styles from './shop.module.css';
 
 export default function Shop() {
   return (
     <div>
-      <h1>Shop</h1>
-      <Outlet />
+      <h1 className="centertext">Shop</h1>
+      <div className={styles.gridContainer}>
+        <nav className={styles.storeNav}>
+          <Link to="/shop">Home</Link>
+          <Link to="/shop/store">Store</Link>
+          <Link to="/shop/basket">Basket</Link>
+        </nav>
+        <Outlet />
+      </div>
     </div>
   );
 }
