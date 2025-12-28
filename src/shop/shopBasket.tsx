edaff7347b3a7cf095d37c34e35fbbfaad7bee5e
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-12-28 16:35:52 +0000

    Order summary

--- src/shop/shopBasket.tsx
+++ src/shop/shopBasket.tsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from 'react-router-dom';
 import { OutletProps } from './shop';
+import styles from './shop.module.css';
 
 export default function ShopBasket() {
   const { newAntiochTeam, basket, setBasket }: OutletProps = useOutletContext();
@@ -43,76 +44,106 @@ export default function ShopBasket() {
     .reduce((partialSum, amt) => partialSum + amt, 0);
 
   return (
-    <div>
+    <div className={styles.basketContainer}>
+      <div className={styles.basketContainerTop}>
+        <picture>
+          <source
+            srcSet="/Link477-React/TrenchCrusade.webp"
+            type="image/webp"
+          />
+          <source srcSet="/Link477-React/TrenchCrusade.jpg" type="image/jpg" />
+          <img
+            src="/Link477-React/TrenchCrusade.jpg"
+            alt="Trench Crusade New Antioch Warband."
+            height={140}
+            width="99%"
+          />
+        </picture>
+      </div>
       <h2>Basket</h2>
+      <p>Select your new war band.</p>
       {Object.keys(basket).map((b) => {
         const item = newAntiochTeam.filter((i) => i.name === b)[0];
         return (
           <div key={b}>
-            <h3>{b}</h3>
-            <picture>
-              <source
-                srcSet={`/Link477-React/${item.path}.webp`}
-                type="image/webp"
-              />
-              <source
-                srcSet={`/Link477-React/${item.path}.jpg`}
-                type="image/jpg"
-              />
-              <img
-                height={200}
-                width={125}
-                src={`/Link477-React/${item.path}.jpg`}
-                alt={b}
-              />
-            </picture>
-            <br />
-            <label>
-              Quantity:
-              <input
-                name="quantity"
-                placeholder="Quantity"
-                value={basket[b]}
-                className="form-control"
-                min={0}
-                max={1000}
-                onChange={(e) => updateBasket(e.target.value, b, 0)}
-              />
-            </label>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => updateBasket('', b, 1)}
-            >
-              +
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => updateBasket('', b, -1)}
-            >
-              -
-            </button>
-            <button
-              type="button"
-              className="btn btn-danger"
-              onClick={() => deleteFromBasket(b)}
-            >
-              🗑️
-            </button>
-            <p>
-              Amount:{' '}
-              <output htmlFor="quantity">
-                {item.price * (parseInt(basket[b]) ? parseInt(basket[b]) : 0)}
-              </output>{' '}
-              Ducats
-            </p>
+            <div className={styles.basketItem}>
+              <picture>
+                <source
+                  srcSet={`/Link477-React/${item.path}.webp`}
+                  type="image/webp"
+                />
+                <source
+                  srcSet={`/Link477-React/${item.path}.jpg`}
+                  type="image/jpg"
+                />
+                <img
+                  height={64}
+                  width={64}
+                  src={`/Link477-React/${item.path}.jpg`}
+                  alt={b}
+                  className={styles.previewPic}
+                />
+              </picture>
+              <div>
+                <h3>{b}</h3>
+                <p>
+                  <output htmlFor="quantity">
+                    {item.price *
+                      (parseInt(basket[b]) ? parseInt(basket[b]) : 0)}
+                  </output>{' '}
+                  Ducats
+                </p>
+              </div>
+              <details>
+                <summary>Change</summary>
+                <label>
+                  Quantity:
+                  <input
+                    name="quantity"
+                    placeholder="Quantity"
+                    value={basket[b]}
+                    className="form-control"
+                    min={0}
+                    max={1000}
+                    onChange={(e) => updateBasket(e.target.value, b, 0)}
+                  />
+                </label>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => updateBasket('', b, 1)}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => updateBasket('', b, -1)}
+                >
+                  -
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => deleteFromBasket(b)}
+                >
+                  🗑️
+                </button>
+              </details>
+            </div>
           </div>
         );
       })}
       <p>
         <b>Grand Total:</b> {grandTotal} Ducats
       </p>
+      <button type="button" className={`btn ${styles.btnPurple}`}>
+        Proceed to Payment
+      </button>
+      <br />
+      <button type="button" className={`btn ${styles.btnPlain}`}>
+        Cancel Order
+      </button>
     </div>
   );
 }
